1a0b4e8ff4a4a2f0826695da4915a6d9ebcbd66e
Leonardo Alves <leolorena.alves@gmail.com>
2024-09-11 09:48:51 -0300

    initial commit

---
+++ src/components/cabecalho/index.jsx
@@ -0,0 +1,16 @@
+import "./index.scss"
+
+export default function Cabecalho() {
+  return (
+    <div className="comp-cabecalho">
+      <img src="/assets/images/logo.svg" alt="" />
+      <div>
+        <nav>
+          <p></p>
+          <p></p>
+          <p></p>
+        </nav>
+      </div>
+    </div>
+  )
+}
---
+++ src/home/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+export default function App() {
+  return (
+    <div className="pagina-home">
+      <h1> DIGITAL TWIN</h1>
+      <h1> OF REVOLUTION </h1>
+    </div>
+  );
+}
+
+
