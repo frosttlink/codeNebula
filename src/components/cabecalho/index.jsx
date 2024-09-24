a329b321d341824786a8f303478b4664209e280f
frosttlink <leolorena.alves@gmail.com>
2024-09-24 11:59:57 -0300

    more commits

--- src/components/cabecalho/index.jsx
+++ src/components/cabecalho/index.jsx
@@ -4,12 +4,12 @@ import "./index.scss"
 export default function Cabecalho() {
   return (
     <div className="comp-cabecalho">
-      <img src="/assets/images/logo.svg" alt="" />
+      <Link to='/'><img src="/assets/images/logo.svg" alt="" /></Link>
       <div>
         <nav>
-          <p>Projetos</p>
-          <p>Fale Conosco</p>
-          <p>Serviços</p>
+          <Link to='/' className="cab-link">Projetos</Link>
+          <Link to='/' className="cab-link">Serviços</Link>
+          <Link to='/' className="cab-link">Fale Conosco</Link>
         </nav>
       </div>
     </div>
--- src/home/App.jsx
+++ src/home/App.jsx
@@ -1,7 +1,8 @@
-import './App.scss';
-import Cabecalho from '../components/cabecalho';
-import { Laptop } from 'lucide-react';
-import Rodape from '../components/rodape';
+import "./App.scss";
+import Cabecalho from "../components/cabecalho";
+import { Laptop } from "lucide-react";
+import Rodape from "../components/rodape";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
@@ -13,68 +14,108 @@ export default function App() {
           <div className="propaganda">
             <h1> DIGITAL TWIN</h1>
             <h1> OF REVOLUTION </h1>
-            <p className='prop-tex'>
-              Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.
+            <p className="prop-tex">
+            Codenebula é uma empresa de tecnologia que desenvolve soluções digitais personalizadas, focando em software, design responsivo e otimização de sistemas. Nossa equipe talentosa transforma ideias em produtos inovadores, garantindo alto desempenho e segurança. 
             </p>
           </div>
           <img src="/assets/images/hexagon.svg" alt="" />
         </div>
-        <img className='line' src="/assets/images/line_two.svg" alt="" />
+        <img className="line" src="/assets/images/line_two.svg" alt="" />
       </section>
-      <section className='segunda'>
+      <section className="segunda">
         <h1>Serviços</h1>
-        <p className='into'>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+        <p className="into">
+        Transformando Ideias em Soluções Tecnológicas que Potencializam Seu Negócio e Enriquecem a Experiência do Usuário.
+        </p>
 
         <div className="cards">
           <div className="card">
-            <Laptop className='icon-laptop' />
+            <Laptop className="icon-laptop" />
             <h3>Website</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+            <p>
+            Nosso serviço de criação de websites oferece soluções digitais personalizadas, com design responsivo e otimização para todos os dispositivos. Garantimos uma experiência de usuário intuitiva, além de implementar práticas de SEO para melhorar a visibilidade online. Entregamos sites que atendem às necessidades dos clientes e superam suas expectativas.
+            </p>
           </div>
           <div className="card">
-            <Laptop className='icon-laptop' />
-            <h3>Website</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+            <Laptop className="icon-laptop" />
+            <h3>Desenvolvimento de Aplicativos Móveis</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim
+              mauris eget amet at proin. Gravida augue interdum nullam laoreet
+              convallis urna. Enim commodo egestas pulvinar accumsan risus non.
+              Ac id adipiscing ut velit.
+            </p>
           </div>
           <div className="card">
-            <Laptop className='icon-laptop' />
+            <Laptop className="icon-laptop" />
             <h3>Website</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim
+              mauris eget amet at proin. Gravida augue interdum nullam laoreet
+              convallis urna. Enim commodo egestas pulvinar accumsan risus non.
+              Ac id adipiscing ut velit.
+            </p>
           </div>
         </div>
 
-        <button>Contatar</button>
+        <button>
+          <Link to="/" className="link-cards-geral">
+            Contatar
+          </Link>
+        </button>
 
-        <img className='line' src="/assets/images/line_two.svg" alt="" />
-
-
+        <img className="line" src="/assets/images/line_two.svg" alt="" />
       </section>
-      <section className='terceira'>
+      <section className="terceira">
         <h1>Projetos</h1>
-        <p className='into'>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+        <p className="into">
+          Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris
+          eget amet at proin. Gravida augue interdum nullam laoreet convallis
+          urna. Enim commodo egestas pulvinar accumsan risus non. Ac id
+          adipiscing ut velit.
+        </p>
 
         <div className="cards">
           <div className="card">
-            <Laptop className='icon-laptop' />
+            <Laptop className="icon-laptop" />
             <h3>Rafaella Designers</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
-            <div className='square'/>
-            <center><button>Ver Link</button></center>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim
+              mauris eget amet at proin. Gravida augue interdum nullam laoreet
+              convallis urna. Enim commodo egestas pulvinar accumsan risus non.
+              Ac id adipiscing ut velit.
+            </p>
+            <div className="square" />
+            <center>
+              <button>
+                <Link to="/" className="link-cards-geral">
+                  Ver Link
+                </Link>
+              </button>
+            </center>
           </div>
           <div className="card">
-            <Laptop className='icon-laptop' />
+            <Laptop className="icon-laptop" />
             <h3>Rafaella Designers</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
-            <div className='square'/>
-            <center><button>Ver Link</button></center>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim
+              mauris eget amet at proin. Gravida augue interdum nullam laoreet
+              convallis urna. Enim commodo egestas pulvinar accumsan risus non.
+              Ac id adipiscing ut velit.
+            </p>
+            <div className="square" />
+            <center>
+              <button>
+                <Link to="/" className="link-cards-geral">
+                  Ver Link
+                </Link>
+              </button>
+            </center>
           </div>
         </div>
-
       </section>
 
       <Rodape />
     </div>
   );
 }
-
-
