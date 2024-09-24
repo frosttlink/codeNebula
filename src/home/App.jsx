e5e88d5b8d4ed9300cb72b75565da573b0736b6b
Leonardo Alves <leolorena.alves@gmail.com>
2024-09-24 09:44:13 -0300

    "finish" commit

--- src/home/App.jsx
+++ src/home/App.jsx
@@ -1,21 +1,78 @@
 import './App.scss';
 import Cabecalho from '../components/cabecalho';
+import { Laptop } from 'lucide-react';
+import Rodape from '../components/rodape';
 
 export default function App() {
   return (
     <div className="pagina-home">
-      <Cabecalho/>
+      <Cabecalho />
+
       <section className="primeira">
-        <div className="propaganda">
-          <h1> DIGITAL TWIN</h1>
-          <h1> OF REVOLUTION </h1>
-          <p className='prop-tex'>
-            Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.
-          </p>
+        <div className="intro">
+          <div className="propaganda">
+            <h1> DIGITAL TWIN</h1>
+            <h1> OF REVOLUTION </h1>
+            <p className='prop-tex'>
+              Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.
+            </p>
+          </div>
+          <img src="/assets/images/hexagon.svg" alt="" />
         </div>
-        <img src="/assets/images/hexagon.svg" alt="" />
+        <img className='line' src="/assets/images/line_two.svg" alt="" />
       </section>
-        <img src="/assets/images/line_one.svg" alt="" />
+      <section className='segunda'>
+        <h1>Serviços</h1>
+        <p className='into'>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+
+        <div className="cards">
+          <div className="card">
+            <Laptop className='icon-laptop' />
+            <h3>Website</h3>
+            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+          </div>
+          <div className="card">
+            <Laptop className='icon-laptop' />
+            <h3>Website</h3>
+            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+          </div>
+          <div className="card">
+            <Laptop className='icon-laptop' />
+            <h3>Website</h3>
+            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+          </div>
+        </div>
+
+        <button>Contatar</button>
+
+        <img className='line' src="/assets/images/line_two.svg" alt="" />
+
+
+      </section>
+      <section className='terceira'>
+        <h1>Projetos</h1>
+        <p className='into'>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+
+        <div className="cards">
+          <div className="card">
+            <Laptop className='icon-laptop' />
+            <h3>Rafaella Designers</h3>
+            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+            <div className='square'/>
+            <center><button>Ver Link</button></center>
+          </div>
+          <div className="card">
+            <Laptop className='icon-laptop' />
+            <h3>Rafaella Designers</h3>
+            <p>Lorem ipsum dolor sit amet consectetur. Pharetra tincidunt enim mauris eget amet at proin. Gravida augue interdum nullam laoreet convallis urna. Enim commodo egestas pulvinar accumsan risus non. Ac id adipiscing ut velit.</p>
+            <div className='square'/>
+            <center><button>Ver Link</button></center>
+          </div>
+        </div>
+
+      </section>
+
+      <Rodape />
     </div>
   );
 }
